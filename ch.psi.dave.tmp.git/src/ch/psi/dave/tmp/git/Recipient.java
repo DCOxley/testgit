5429ef604c7f2e079f8daf46c7f91daedda5f9e6
oxleyd <oxleyd@pc11450.psi.ch>
2019-07-25 16:04:59 +0200

    add recipient

---
+++ ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/Recipient.java
@@ -0,0 +1,18 @@
+package ch.psi.dave.tmp.git;
+
+public class Recipient {
+	private final String name;
+	
+	
+	public Recipient() {
+		name = "git";
+	}
+
+
+	public Recipient(String name) {
+		super();
+		this.name = name;
+	}
+	
+	
+}
