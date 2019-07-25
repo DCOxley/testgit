40c369288415ed249ab5d6210b0f77466a449d4a
oxleyd <oxleyd@pc11450.psi.ch>
2019-07-25 16:57:22 +0200

    add language

---
+++ ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/Language.java
@@ -0,0 +1,13 @@
+package ch.psi.dave.tmp.git;
+
+public class Language {
+	private final String languageName;
+
+	public Language() {
+		languageName = "language not set";
+	}
+
+	public Language(String languageName) {
+		this.languageName = languageName;
+	}
+}
