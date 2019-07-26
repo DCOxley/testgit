bdb44862d8e53d72e50255efec2f8ad0963a5cef
oxleyd <oxleyd@pc11450.psi.ch>
2019-07-26 14:03:59 +0200

    refactor speaker to greet

--- ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/HelloGit.java
+++ ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/HelloGit.java
@@ -5,11 +5,14 @@ public class HelloGit {
 	private static final String NICE_DAY = "I hope you have a nice ";
 
 	public static void main(String[] args) {
+		Speaker speaker = new Speaker();
 		Salutation salutation = new Salutation("Howdie ");
+		speaker.greet(salutation);
 		System.out.println(salutation.getSalutationCall() + "Git!");
 		Day day = new Day();
 
 		System.out.println(NICE_DAY + day.getDayName());
 		System.out.println("Goodbye Git!");
 	}
+
 }
---
+++ ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/Speaker.java
@@ -0,0 +1,10 @@
+package ch.psi.dave.tmp.git;
+
+public class Speaker {
+	public void greet(Salutation salutation) {
+		System.out.println(salutation.getSalutationCall() + "Git!");
+	}
+	public void sayGoodbye() {
+		
+	}
+}
