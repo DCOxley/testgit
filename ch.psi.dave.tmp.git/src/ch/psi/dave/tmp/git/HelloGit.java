7208a2ceec8a904928cc28da21be4e2a537d453f
oxleyd <oxleyd@pc11450.psi.ch>
2019-07-26 13:53:44 +0200

    add nice day constant

--- ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/HelloGit.java
+++ ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/HelloGit.java
@@ -2,11 +2,14 @@ package ch.psi.dave.tmp.git;
 
 public class HelloGit {
 
+	private static final String NICE_DAY = "I hope you have a nice ";
+
 	public static void main(String[] args) {
 		Salutation salutation = new Salutation("Howdie ");
-		System.out.println(salutation.getSalutationCall()+"Git!");
+		System.out.println(salutation.getSalutationCall() + "Git!");
 		Day day = new Day();
-		System.out.println(day.getDayName());
+
+		System.out.println(NICE_DAY + day.getDayName());
 		System.out.println("Goodbye Git!");
 	}
 }
