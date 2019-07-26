c37e0bd8b8022b5d78a215b13dbcad88dbb7fbdf
oxleyd <oxleyd@pc11450.psi.ch>
2019-07-26 13:19:08 +0200

    merge main from master

--- ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/HelloGit.java
+++ ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/HelloGit.java
@@ -4,7 +4,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		Day day = new Day();
+		System.out.println(day.getDayName());
 		System.out.println("Goodbye Git!");
-		//Recipient recipient = new Recipient("world");
 	}
 }
