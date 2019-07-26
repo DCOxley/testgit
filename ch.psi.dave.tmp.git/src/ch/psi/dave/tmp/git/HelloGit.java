e7bf0bd06ae159434ee8d9452194a8b1b8372269
oxleyd <oxleyd@pc11450.psi.ch>
2019-07-26 14:58:06 +0200

    merge main

--- ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/HelloGit.java
+++ ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/HelloGit.java
@@ -2,10 +2,18 @@ package ch.psi.dave.tmp.git;
 
 public class HelloGit {
 
+	private static final String NICE_DAY = "I hope you have a nice ";
+
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		Speaker speaker = new Speaker();
+		Salutation salutation = new Salutation("Howdie ");
+
+		speaker.greet(salutation);
+
 		Day day = new Day();
-		System.out.println(day.getDayName());
-		System.out.println("Goodbye Git!");
+		System.out.println(NICE_DAY + day.getDayName());
+
+		speaker.sayGoodbye();
 	}
+
 }
