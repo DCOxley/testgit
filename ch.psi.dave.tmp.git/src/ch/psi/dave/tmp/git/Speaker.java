105c5e6225617d00e27589424ffb7d0bee27d3af
oxleyd <oxleyd@pc11450.psi.ch>
2019-07-26 14:44:46 +0200

    speaker interface with greet and say goodbye

--- ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/Speaker.java
+++ ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/Speaker.java
@@ -1,10 +1,10 @@
 package ch.psi.dave.tmp.git;
 
-public class Speaker {
+public class Speaker implements ISpeaker {
 	public void greet(Salutation salutation) {
 		System.out.println(salutation.getSalutationCall() + "Git!");
 	}
 	public void sayGoodbye() {
-		
+		System.out.println("Goodbye Git!");
 	}
 }
