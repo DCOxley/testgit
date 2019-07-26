175812fd6e4ac45998d6efe785eb64d3f826a6de
oxleyd <oxleyd@pc11450.psi.ch>
2019-07-26 14:44:46 +0200

    speaker interface with greet and say goodbye

--- ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/HelloGit.java
+++ ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/HelloGit.java
@@ -7,12 +7,13 @@ public class HelloGit {
 	public static void main(String[] args) {
 		Speaker speaker = new Speaker();
 		Salutation salutation = new Salutation("Howdie ");
-		speaker.greet(salutation);
-		System.out.println(salutation.getSalutationCall() + "Git!");
-		Day day = new Day();
 
+		speaker.greet(salutation);
+
+		Day day = new Day();
 		System.out.println(NICE_DAY + day.getDayName());
-		System.out.println("Goodbye Git!");
+
+		speaker.sayGoodbye();
 	}
 
 }
---
+++ ch.psi.dave.tmp.git/src/ch/psi/dave/tmp/git/ISpeaker.java
@@ -0,0 +1,6 @@
+package ch.psi.dave.tmp.git;
+
+public interface ISpeaker {
+	public void greet(Salutation salutation);
+	public void sayGoodbye();
+}
